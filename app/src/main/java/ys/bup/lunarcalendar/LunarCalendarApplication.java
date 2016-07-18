82ef0c48a72f9e7a7f7f1152c90d144d9cc8129a
YS <yoonsam86@gmail.com>
2016-07-18 17:11:14 +0900

    Update LunarCalendarApplication.java
    apply to dagger2

--- app/src/main/java/ys/bup/lunarcalendar/LunarCalendarApplication.java
+++ app/src/main/java/ys/bup/lunarcalendar/LunarCalendarApplication.java
@@ -10,12 +10,30 @@ import io.realm.RealmConfiguration;
  */
 public class LunarCalendarApplication extends Application {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+    private ApplicationComponent component;
 
-        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this).deleteRealmIfMigrationNeeded().build();
-        Realm.setDefaultConfiguration(realmConfiguration);
+	@Component(modules = AppModule.class)
+	public interface ApplicationComponent {
+		void inject(LunarCalendarApplication application);
+		void inject(LunarMainAt mainActivity);
+	}
 
-    }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		initApplicationComponent();
+	}
+
+	private void initApplicationComponent() {
+		this.component = DaggerLunarCalendarApplication_ApplicationComponent.builder()
+				.applicationModule(new AppModule(this))
+				.build();
+		
+		component.inject(this);
+	}
+
+	public ApplicationComponent getApplicationComponent() {
+		return this.component;
+	}
 }
